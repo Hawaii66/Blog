7c54ea219e65af1751e4573bee0b89cf3a34b5c3
Sebastian Ahlman <hawaiilive@outlook.com>
2022-01-30 19:55:50 +0100

    Acctual links

--- Client/blog/src/Contexts/StaticContext.ts
+++ Client/blog/src/Contexts/StaticContext.ts
@@ -7,7 +7,7 @@ interface StaticContextInterface {
 
 export const StaticContext = createContext<StaticContextInterface>(
     {
-        apiEndPoint:"http://localhost:5000",
-        website:"http://localhost:3000"//"https://www.blog.hawaiidev.net"
+        apiEndPoint:/*"http://localhost:5000"*/"https://hawaii-dev-blog.herokuapp.com",
+        website:/*"http://localhost:3000"//*/"https://www.blog.hawaiidev.net"
     }
 );
