2c5a0634d4e3573eec91a967a96304dddf1db4b3
Sebastian Ahlman <hawaiilive@outlook.com>
2022-01-30 15:05:51 +0100

    No slash at end

--- Client/blog/src/Contexts/StaticContext.ts
+++ Client/blog/src/Contexts/StaticContext.ts
@@ -7,7 +7,7 @@ interface StaticContext {
 
 export const StaticContext = createContext<StaticContext>(
     {
-        apiEndPoint:"https://hawaii-dev-blog.herokuapp.com/",
-        website:"https://www.blog.hawaiidev.net/"
+        apiEndPoint:"https://hawaii-dev-blog.herokuapp.com",
+        website:"https://www.blog.hawaiidev.net"
     }
 );
